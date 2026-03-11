14dec85b00502ecbad32826a0e000c378e98606f
Tkahashichika0104 <kk.cc.9225.clover.9480@gmail.com>
2026-03-11 22:55:33 +0900

    ログイン機能の実装：AuthControllerとログインページの追加

---
+++ kadai_002/src/main/java/com/example/nagoyameshi/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.example.nagoyameshi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    //ログインページ表示
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+    }
+
+}
